4dfb5a59a0f0f8f8a44943066ce2c27f6f88efcb
권정 <48792069+kwonjeong@users.noreply.github.com>
2021-01-09 16:30:35 +0900

    UNIT 13 task

---
+++ 13_07.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+
+int main() {
+    int num1;
+    float num2;
+    char c1;
+
+    scanf("%d %f %c", &num1, &num2, &c1);
+
+    num1 += 1;
+    num2 += 1;
+    c1 -= 1;
+
+    printf("%d %f %c\n", num1, num2, c1);
+
+    return 0;
+}
